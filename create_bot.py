e498defa2b83002d95e367c283036269cbbbe2c6
Kanagat01 <skanagat101@gmail.com>
2024-05-20 18:08:17 +0500

    changed tz

--- create_bot.py
+++ create_bot.py
@@ -26,7 +26,6 @@ dp = Dispatcher()
 
 calendar_id = config.tg_bot.calendar_id
 local_tz = 'Europe/Moscow'
-# local_tz = 'Etc/GMT-5'
 local_tz_obj = timezone(local_tz)
 scheduler = AsyncIOScheduler(timezone=local_tz_obj)
 scheduler.add_jobstore('redis', jobs_key='example.jobs',
